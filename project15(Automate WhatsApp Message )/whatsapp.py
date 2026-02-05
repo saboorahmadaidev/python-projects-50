f4960c2e2e0a44ec741a4a6d44f85d72bab1c389
SABOOR AHMAD <saboorahmad.aidev@gmail.com>
2026-02-05 22:59:11 +0500

    Automate Whatsapp Message

---
+++ project15(Automate WhatsApp Message )/whatsapp.py
@@ -0,0 +1,6 @@
+import pywhatkit as pyw
+# pyw.sendwhatmsg(phone_no, message, time_hour, time_min, wait_time=20, print_waitTime=True)
+# pyw.sendwhatmsg_instantly(phone_no, message, wait_time=20,
+# print_waitTime=True)
+# pyw.sendwhats_image(phone_no, img_path, caption=None, wait_time=20,
+# print_waitTime=True)
